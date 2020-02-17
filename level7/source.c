5c57ab2bcbe0189525b8277835d30d27905b3d87
Amina MANSOUR <amansour@e1r2p1.42.fr>
2020-02-17 16:13:13 +0100

    level7 fixed

---
+++ level7/source.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+char [100]c; //a checker
+
+void m(void)
+{
+	printf("%s - %d\n", c, time(0));
+	return ;
+}
+
+int main(int argc, char **argv)
+{
+	char *s1 = malloc(8);
+	*s1 = 1;
+	char *s2 = malloc(8);
+	*s2 = 2;
+	strcpy(s1, argv[1]);
+	strcpy(s2, argv[2]);
+	File *f = fopen("/home/user/level8/.pass", "r");
+	fgets(c, 68, f);
+	fputs("~~");
+	return (0);
+}

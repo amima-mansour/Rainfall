e8210285d11ccd7f1fc39e5691978c84f9ac2c01
Amina MANSOUR <amansour@e1r3p7.42.fr>
2020-03-09 11:50:00 +0100

    level3 fixed

--- level3/source.c
+++ level3/source.c
@@ -1,22 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int m; //global variable (address: 0x804988c)
+int m;
 
 void v()
 {
-	//0x208
 	char[520] s;
 
-	//0x200
-	fgets(buff, 512, stdin)
-	printf(buff)
-
-	//0x40
+	fgets(buff, 512, stdin);
+	printf(buff);
 	if (m == 64)
 	{
-		//fwrite( buffer, BLOCK_SIZE, 1, stream)
-		fwrite("Whait what?!\n", 1, 12, stdout);
+		fwrite("Wait what?!\n", 1, 12, stdout);
 		system("/bin/sh");
 	}
 	return;

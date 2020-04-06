7f66929a0b5863cafd86c14d8835d970185f1a03
amansour <amansour@MacBook-Air-de-amansour.local>
2020-04-06 16:52:58 +0200

    bonus0 fixed

--- bonus0/source.c
+++ bonus0/source.c
@@ -3,33 +3,31 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-char *p(char *str1, char *str2)
+void p(char *buf, char *str)
 {
-	char [4104]str3;
-	
-	puts(str1);
-	read(0, &str2, 4096);
-	*strchr(str1, 10) = 0;
-	return strncpy(str1, str3, 20);
+	char str2[4104];
+
+	puts(str);
+	read(0, str2, 4096);
+	*strchr(str1, 10) = 0; // 10 = '\n'
+	strncpy(buf, str2, 20);
 }
 
 void pp(char *str)
 {
-	char [20]str1; //x30 - x1c = 48 - 28 = 20 
-	char [20]str2; //x1c - x08 = 28 - 8 = 20
+	char [20]str1;
+	char [28]str2;
 	
 	p(str1, " - ");
 	p(str2, " - ");
 	strcpy(str, str1);
-	str[strlen(str)] = 32; //to check (gdb) x/x 0x80486a4
-						   //0x80486a4:	0x20 = 32
+	str[strlen(str)] = 32; // 32 = ' '
 	strcat(str, str2);
-	return (str);
 }
 
 int main(void)
 {
-	char [42]str; //64 - 22
+	char [42]str;
 
 	pp(str);
 	puts(str);
